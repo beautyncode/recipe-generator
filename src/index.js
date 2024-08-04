f4a51306328af3638d43c242bd4041e6285eefa4
Kenya M <beautyncode@users.noreply.github.com>
2024-08-04 13:01:47 -0400

    Updated Context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "dcbe9afdd9023a013ceaof19ted4923f";
   let context =
-    "You love to cook and you have an endless supply of recipes to share with others. Your mission is to generate a simple and easy to follow recipe based on the instructions provided. The recipe should be specifically tailored to the instructions and should include step-by-step instructions in HTML format, such as using <p> tags for each step. Sign the recipe with 'AI Integrations by Kenyata Walls' inside a <strong> element.";
+    "You love to cook and you have an endless supply of recipes to share with others. Your mission is to generate a simple and easy to follow recipe based on the instructions provided. The recipe should be specifically tailored to the instructions and should include step-by-step instructions in HTML format, such as using <p> tags for each step. Sign the recipe with 'AI API Integrations by Kenyata Walls' inside a <strong> element.";
   let prompt = `Generate a recipe for "${instructionsInput.value}".`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
